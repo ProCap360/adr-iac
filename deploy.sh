88ba51e413b425e2f690338a5d5574386999eba1
Alan Chan <alanchan@iis-corp.com>
2024-06-05 14:34:40 -0400

    fix workspace using subdomain

--- deploy.sh
+++ deploy.sh
@@ -13,4 +13,4 @@ fi
 PATH=$PATH:~/.local/bin
 export ANSIBLE_HOST_KEY_CHECKING=False
 ansible-playbook --connection=local --inventory 127.0.0.1, playbook_terraform.yaml --extra-vars subdomain=$subdomain || error Failed to complete playbook_terraform.yaml
-ansible-playbook --private-key ./ssh.pem -i $(terraform output -raw vm_ip_address), -u "$subdomain-admin" playbook.yaml || error Failed to complete playbook.yaml
+ansible-playbook --private-key ./ssh.pem -i $(terraform output -raw vm_ip_address), -u "$subdomain-admin" playbook.yaml --extra-vars subdomain=$subdomain || error Failed to complete playbook.yaml
